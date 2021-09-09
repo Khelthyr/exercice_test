d519eb5f82e874150d967897e776ca896410e583
Khelthyr <cedric.nils.vincendeau@gmail.com>
2021-09-09 14:51:26 +0200

    end of exo3

--- src/Exercice3/Exercice.php
+++ src/Exercice3/Exercice.php
@@ -7,22 +7,26 @@ class Balanced
 {
 	public static function balance(string $text)
 	{
-		static $open = 0;
-		if ((substr($text, 0,1) == ")") || (substr($text, -1,0) == "(")){
-			return false;
-		}
-
-		for ($i = 0; $i < strlen($text) ; $i++) { 
-			if (substr($text, $i,1) == ")") {
-				$open++;
+		$open = 0;
+		for ($i = 0; $i < strlen($text) ; $i++) {
+			var_dump($open); 
+			if (substr($text, $i, 1) == "(") {
+				$open+=1;
 			}
-			else{
+			elseif (substr($text, $i, 1) == ")") {
+				$open-=1;
 				if ($open < 0) {
 					return false;
 				}
-				$open--;
+			}
+			else {
+				if ($open < 0) {
+					return false;
+				}
 			}
 		}
-		return true;
+		if ($open === 0) {
+			return true;
+		}
 	}
 }
--- tests/Exercice3Test.php
+++ tests/Exercice3Test.php
@@ -4,23 +4,35 @@ use PHPUnit\Framework\TestCase;
 
 final class Exercice3Test extends TestCase
 {
-    public function testOne()
+    public function test1()
     {
-        $this->assertEquals(true, Balanced::balance("()"));
+        $this->assertEquals(true, Balanced::balance('(())'));
     }
 
-    public function testTwo()
+    public function test2()
     {
-        $this->assertEquals(true, Balanced::balance("(foo(bar))"));
+        $this->assertEquals(true, Balanced::balance('(foo(bar))'));
     }
 
-    public function testThree()
+    public function test3()
     {
-        $this->assertEquals(false, Balanced::balance(")()()"));
+        $this->assertEquals(true, Balanced::balance('foo'));
     }
 
-    public function testFour()
+    public function test4()
     {
-        $this->assertEquals(false, Balanced::balance("()()("));
+        $this->assertEquals(false, Balanced::balance('())'));
     }
+
+    public function test5()
+    {
+        $this->assertEquals(false, Balanced::balance('((foo)'));
+    }
+
+    public function test6()
+    {
+        $this->assertEquals(false, Balanced::balance(')(()'));
+    }
+
+    
 }
